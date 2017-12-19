21086d7b6b32f5aea340a867323a8a614a156e61
wonchangkim <wonchang.k@gmail.com>
2017-12-19 17:02:48 +0900

    rgb update

--- rgb/js/index.js
+++ rgb/js/index.js
@@ -1,1 +1,11 @@
-alert('hello');
+function random255(){
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor(){
+  return `rgb(${random255()},${random255()},${random255()})`
+}
+
+document.querySelectorAll('.box').forEach(el =>{
+  el.style.backgroundColor = randomColor();
+})

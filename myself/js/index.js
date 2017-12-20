9678cf06c75049da656ad70092b113c3c1848cf3
wonchangkim <wonchang.k@gmail.com>
2017-12-20 19:04:45 +0900

    myself update

--- myself/js/index.js
+++ myself/js/index.js
@@ -1,7 +1,27 @@
-function Enter_Check() {
-    // 엔터키의 코드는 13입니다.
-    if (event.keyCode == 13) {
-        console.log('hh'); // 실행할 이벤트
-        
-    }
+
+let d = new Date();
+let h = d.getHours();
+let m = d.getMinutes();
+let timeset = '';
+let nowtime = 24 - h;
+
+if(h < 12 && h > 0){
+    timeset = '오전' + nowtime;
+    
+}else{
+    timeset = '오후' + nowtime;
 }
+
+const chatarea = document.querySelector('.chat-messages-list');
+
+
+function getTxt() {
+   
+    let inputrequest = document.querySelector('#inputres').value;
+    let question = `<div class="restxt right">
+                            <span class="txttime">${timeset}:${m}</span>
+                            <span id="request" class="anwtext reschatbubble">${inputrequest}</span>
+                    </div>`
+    chatarea.innerHTML = question;
+}
+

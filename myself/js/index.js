b2fc8fd9f5588fe759aa23549b30fe75a7b78f7f
wonchangkim <wonchang.k@gmail.com>
2017-12-21 17:03:49 +0900

    update

--- myself/js/index.js
+++ myself/js/index.js
@@ -1,27 +1,43 @@
+// let d = new Date();
+// let h = d.getHours();
+// let m = d.getMinutes();
+// let timeset = '';
+// let aftertime = h - 12
 
-let d = new Date();
-let h = d.getHours();
-let m = d.getMinutes();
-let timeset = '';
-let nowtime = 24 - h;
+// function addZero(i) {
+//     if (i < 10) {
+//         i = "0" + i;
+//     }
+//     return i;
+// }
 
-if(h < 12 && h > 0){
-    timeset = '오전' + nowtime;
-    
-}else{
-    timeset = '오후' + nowtime;
-}
+// function myFunction() {
+//     var m = addZero(d.getMinutes());
+// }
+
+// if (h > 0 && h < 12) {
+//     timeset = '오전' + h;
+// } else {
+//     timeset = '오후' + aftertime;
+// }
+const moment = require("moment")
+moment.locale('ko');
+const nowtime = moment().format('LT'); 
 
 const chatarea = document.querySelector('.chat-messages-list');
 
-
-function getTxt() {
-   
-    let inputrequest = document.querySelector('#inputres').value;
-    let question = `<div class="restxt right">
-                            <span class="txttime">${timeset}:${m}</span>
-                            <span id="request" class="anwtext reschatbubble">${inputrequest}</span>
-                    </div>`
-    chatarea.innerHTML = question;
+function adddiv() {
+    let makediv = document.createElement('div');
+    makediv.id = 'wrap';
+    chatarea.appendChild(makediv);
 }
 
+
+
+function getTxt() {
+    console.log('make a div');
+    let inputrequest = document.querySelector('#inputres').value;
+    let text = `<span class="txttime">${nowtime}</span>
+             <span id="request" class="anwtext reschatbubble">${inputrequest}</span>              `
+    document.querySelector('.wrap').innerHTML = text;
+}
--- rgb/js/index.js
+++ rgb/js/index.js
@@ -1,15 +1,53 @@
-function random255(){
-
+function random255() {
   return Math.floor(Math.random() * 256);
 }
 
-function randomColor(){
+function randomColor() {
   return `rgb(${random255()},${random255()},${random255()})`
 }
 
-document.querySelectorAll('.box').forEach(el =>{
-  el.style.backgroundColor = randomColor();
-})
-const el = document.querySelector('.box')
 
-el.eventListener('click',  )
+
+let stage;
+let problem;
+let correctAnswer;
+
+function nextStage() {
+  stage++;
+  problem = [randomColor(), randomColor(), randomColor()];
+  correctAnswer = Math.floor(Math.random() * 3);
+
+}
+
+function init(){
+  stage = 0;
+  problem = [randomColor(), randomColor(), randomColor()];
+  correctAnswer = Math.floor(Math.random()*3);
+
+}
+
+function draw() {
+  document.querySelectorAll('.box').forEach((el, index) => {
+    el.style.backgroundColor = problem[index];
+  })
+  document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+  document.querySelector('.score').textContent = stage;
+}
+
+document.querySelectorAll('.box').forEach((el, index) => {
+  el.addEventListener('click', e => {
+    if (index === correctAnswer) {
+      nextStage();
+      draw();
+    } else {
+      init();
+      draw();
+    }
+  });
+})
+
+init()
+
+draw();
+
+

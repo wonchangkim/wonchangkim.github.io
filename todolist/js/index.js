fc2812dc81c3bd000fa2f6fa18fa468c0bc83f16
kim wonchang <wonchang.k@gmail.com>
2017-12-23 15:27:36 +0900

    form reset

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,14 +1,40 @@
 //add버튼을 눌렀을때 새로운 엘리먼트에 넣어주고 보여주기
 const addButtonEl = document.querySelector('#add-button')
-
+const formEl = document.querySelector('#todo-form');
 const inputEl = document.querySelector('#todo-input');
 const listEl = document.querySelector('#todo-list');
 
-addButtonEl.addEventListener('click', e => {
-  //F2 누르고 바꾸기
-  const itemEl = document.createElement('div');
 
-  itemEl.textContent = inputEl.value;
-  listEl.appendChild(itemEl);
-  inputEl.value = '';
-});
+
+function add(){
+
+  const itemEl = document.createElement('div');
+//빈값체크
+  if(inputEl.value == ""){
+    alert("할일을 입력하세요");
+
+  }else{
+    itemEl.textContent = inputEl.value;
+    listEl.appendChild(itemEl);
+    formEl.reset(); //form reset
+  }
+
+  itemEl.addEventListener('click', e=>{
+    if(itemEl.classList.contains('complete')){
+      itemEl.classList.remove('complete')
+    }else{
+      itemEl.classList.add('complete');
+    }
+  });
+
+   const close = document.createElement('div')
+   itemEl.appendChild(close)
+   close.textContent = 'x';
+
+   close.addEventListener('click', e => {
+   listEl.removeChild(itemEl);
+  });
+}
+
+
+

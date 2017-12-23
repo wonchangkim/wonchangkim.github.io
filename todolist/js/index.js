9a8be7dc6cc4b190a4fe5caf0da59a589cd0a3f1
kim wonchang <wonchang.k@gmail.com>
2017-12-23 19:04:28 +0900

    "시간추가"

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,3 +1,10 @@
+//시간
+moment.locale('ko');
+const nowtime = moment().format('LLL');
+
+
+
+
 //add버튼을 눌렀을때 새로운 엘리먼트에 넣어주고 보여주기
 const addButtonEl = document.querySelector('#add-button')
 const formEl = document.querySelector('#todo-form');
@@ -14,7 +21,7 @@ function add(){
     alert("할일을 입력하세요");
 
   }else{
-    itemEl.textContent = inputEl.value;
+    itemEl.textContent = inputEl.value ;
     listEl.appendChild(itemEl);
     formEl.reset(); //form reset
   }
@@ -26,10 +33,15 @@ function add(){
       itemEl.classList.add('complete');
     }
   });
+  const time = document.createElement('span')
+  itemEl.appendChild(time)
+  time.textContent = nowtime;
 
-   const close = document.createElement('div')
-   itemEl.appendChild(close)
+
+  const close = document.createElement('div')
+  itemEl.appendChild(close)
   close.textContent = String.fromCodePoint(0xD800,0xDD02) ;
+
 
    close.addEventListener('click', e => {
    listEl.removeChild(itemEl);

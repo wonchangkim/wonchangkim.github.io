a46df5037ae7cf6f6f9a99ac874b8edf9e673eea
kim wonchang <wonchang.k@gmail.com>
2017-12-24 01:52:44 +0900

    모바일 레이아웃수정

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -14,37 +14,45 @@ const listEl = document.querySelector('#todo-list');
 
 
 function add(){
-
+  const wrapEl = document.createElement('div');
   const itemEl = document.createElement('div');
+
 //빈값체크
   if(inputEl.value == ""){
     alert("할일을 입력하세요");
 
   }else{
+    listEl.appendChild(wrapEl);
+    wrapEl.classList.add('wrap');
     itemEl.textContent = inputEl.value ;
-    listEl.appendChild(itemEl);
+    wrapEl.appendChild(itemEl);
+    itemEl.classList.add('txt')
     formEl.reset(); //form reset
   }
 
-  itemEl.addEventListener('click', e=>{
-    if(itemEl.classList.contains('complete')){
-      itemEl.classList.remove('complete')
+  wrapEl.addEventListener('click', e=>{
+    if(wrapEl.classList.contains('complete')){
+      wrapEl.classList.remove('complete');
+      itemEl.classList.remove('textdeco');
     }else{
-      itemEl.classList.add('complete');
+      wrapEl.classList.add('complete');
+      itemEl.classList.add('textdeco');
     }
   });
   const time = document.createElement('span')
-  itemEl.appendChild(time)
+  wrapEl.appendChild(time)
+  time.classList.add('padding')
   time.textContent = nowtime;
 
 
   const close = document.createElement('div')
-  itemEl.appendChild(close)
+  close.classList.add('close');
+  wrapEl.appendChild(close)
   close.textContent = String.fromCodePoint(0xD800,0xDD02) ;
 
 
    close.addEventListener('click', e => {
-   listEl.removeChild(itemEl);
+   listEl.removeChild(wrapEl);
   });
 }
 

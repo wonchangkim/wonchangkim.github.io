afa622689589130d731bc55eb3225a4fef5dfd1f
kim wonchang <wonchang.k@gmail.com>
2017-12-28 08:03:46 +0900

    modal&collapse update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
-document.querySelector('.img');
-document.querySelector('carousel-prev');
-document.querySelector('carousel-next');
+function carouselbtn() {
+    let collapse = document.querySelector('.collapse');
+    let collapsecontent = document.querySelector('.collapsecontent');
+ 
+    if (collapse.classList.contains('collapseshow')) {
+        collapse.classList.remove('collapseshow');
+        collapsecontent.classList.remove('collapsecontentshow');
+
+    } else {
+        collapse.classList.add('collapseshow');
+        collapsecontent.classList.add('collapsecontentshow');
+    }
+
+}
+
+
+function modalbtn() {
+    let modalbtn = document.querySelector('.modal-btn');
+    let modalwarp = document.querySelector('.modalwarp');
+    let modal = document.querySelector('.modal');
+
+    if (modal.classList.contains('modalshow')) {
+        modal.classList.remove('modalshow');
+    } else {
+        modal.classList.add('modalshow');
+        modalwarp.classList.add('modalwarpshow');
+    }
+    
+}
+
+function modalclose() {
+    let modalwarp = document.querySelector('.modalwarp');
+    let modal = document.querySelector('.modal');
+    modal.classList.remove('modalshow');
+    modalwarp.classList.remove('modalwarpshow');
+
+}

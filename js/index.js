c7c18d7b5fd5c4df591146348cd4ff782cbbd10a
kim wonchang <wonchang.k@gmail.com>
2017-12-28 18:59:39 +0900

    슬라이더 업데이트

--- js/index.js
+++ js/index.js
@@ -37,12 +37,44 @@ function modalclose() {
 }
 
 
-let dcount = 0;
-let scount = 1;
+let dcount = 1;
+let scount = 2;
 
 
-function showSlides() {
+function prevSlides() {
     let slides = document.querySelectorAll('.img');
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.animation = null;
+        slides[i].style.zIndex = null;
+        slides[i].style.display = 'none';
+    }
+    if (dcount === 2) {
+        scount = 0;
+    }
+    if (scount === 1) {
+        dcount = 0;
+    }
+    console.log(dcount, scount);
+    slides[dcount].style.animation = 'disappearleft 0.5s';
+    slides[dcount].style.zIndex = '0';
+    slides[dcount].style.display = 'block';
+    slides[scount].style.animation = 'showleft 0.5s';
+    slides[scount].style.display = 'block';
+    slides[scount].style.zIndex = '1';
+
+    dcount += 1;
+    scount += 1;
+
+}
+
+function nextSlides() {
+    let slides = document.querySelectorAll('.img');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.animation = null;
+        slides[i].style.zIndex = null;
+        slides[i].style.display = 'none';
+    }
 
     if (dcount === 2) {
         scount = 0;
@@ -51,19 +83,14 @@ function showSlides() {
         dcount = 0;
     }
     console.log(dcount, scount);
-   
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.animation = null;
-        slides[i].style.zIndex = null;
-        slides[i].style.display = 'none';
-    }
-    slides[dcount].style.animation = 'disappear 0.5s';
+
+    slides[dcount].style.animation = 'disappearright 0.5s';
     slides[dcount].style.zIndex = '0';
     slides[dcount].style.display = 'block';
-    slides[scount].style.animation = 'show 0.5s';
+    slides[scount].style.animation = 'showright 0.5s';
     slides[scount].style.display = 'block';
     slides[scount].style.zIndex = '1';
-    
-    dcount++;
-    scount++;
+
+    dcount += 1;
+    scount += 1;
 }

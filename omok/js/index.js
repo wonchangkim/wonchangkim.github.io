2d4b0dadfda2b3a3fd5157c60320479486e2317c
kim wonchang <wonchang.k@gmail.com>
2018-01-01 03:23:18 +0900

    대각선수정

--- omok/js/index.js
+++ omok/js/index.js
@@ -99,14 +99,7 @@ let diagonalb = 1;
 let diagonal2b = 1;
 let whiteSamearry = new Array();
 let sameNum;
-function compareSecondColumn(a, b) {
-    if (a[0] === b[0]) {
-        return 0;
-    }
-    else {
-        return (a[0] < b[0]) ? -1 : 1;
-    }
-}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.col').forEach(el => {
@@ -127,25 +120,29 @@ document.addEventListener('DOMContentLoaded', () => {
                     let wrr = new Array();
                     wrr.push(Number(r), Number(el.classList[0].slice(4, 7)));
                     wrows.push(wrr);
-                    wrows.sort(compareSecondColumn) ;
-                    console.log(wrows);
+                    // judgeCol();
+                    judgediagonal();
+                  
+                    // judgerow();
 
-                    if (wrows.length >= 2) {
-                        let colcount = 1;
-                        for (let i = 0, j = 1; i < wrows.length - 1, j < wrows.length; i++, j++) {
-                            if (wrows[i][0] === wrows[j][0] - 1 && wrows[i][1] === wrows[j][1]) {
-                                colcount++
-                                console.log(colcount);
-                                if (colcount >= 5) {
-                                    alert('게임끝');
-                                }
-                            }else{
-                                colcount = 1;
-                                console.log(colcount);
-                            }
-                        }
-                       
-                    }
+
+                    // if (wrows.length >= 2) {
+                    //     let colcount = 1;
+                    //     for (let i = 0, j = 1; i < wrows.length - 1, j < wrows.length; i++, j++) {
+                    //         if (wrows[i][0] === wrows[j][0] - 1 && wrows[i][1] === wrows[j][1]) {
+                    //             // wrows[]
+                    //             colcount++
+                    //             console.log(colcount);
+                    //             if (colcount >= 5) {
+                    //                 alert('게임끝');
+                    //             }
+                    //         }else{
+                    //             colcount = 1;
+                    //             console.log(colcount);
+                    //         }
+                    //     }
+
+                    // }
 
                 };
                 turn++;
@@ -298,3 +295,179 @@ document.addEventListener('DOMContentLoaded', () => {
 // console.log(`흑돌가로연속값 : ${rowwcountb}`);
 // console.log(`흑돌가로초기값: ${rowwcountb}`);
 // }
+
+
+function judgeCol() {
+    let wcolcount = 1;
+    wrows.sort(function (a, b) {
+        return a[1] - b[1]; //col값정열
+    });
+    const colsamerow = {}; //같은 col 모으기
+
+    for (let c of wrows) {
+        if (colsamerow[c[1]] === undefined) {
+            colsamerow[c[1]] = [];
+        }
+        colsamerow[c[1]].push(c[0]);
+        colsamerow[c[1]].sort(function (a, b) {
+            return a - b;
+        })
+    }
+    for (let item in colsamerow) {
+        for (let i = 0, j = 1; i < colsamerow[item].length - 1, j < colsamerow[item].length; i++, j++) {
+            console.log(colsamerow[item])
+            if (colsamerow[item].length > 4 && colsamerow[item][i] === colsamerow[item][j] - 1) {
+                wcolcount++;
+                console.log(wcolcount);
+                if (wcolcount === 5) {
+                    alert('게임끝');
+                }
+            } else {
+                wcolcount = 1;
+                console.log(wcolcount);
+            }
+        }
+    }
+    console.log(colsamerow);
+    console.log(wrows);
+}
+function judgerow() {
+    let rowwcount = 1;
+    wrows.sort(function (a, b) {
+        return a[1] - b[1]; //col값정열
+    });
+    const rowsamecol = {}; //같은 col 모으기
+
+    for (let c of wrows) {
+        if (rowsamecol[c[1]] === undefined) {
+            rowsamecol[c[1]] = [];
+        }
+        rowsamecol[c[1]].push(c[0]);
+        rowsamecol[c[1]].sort(function (a, b) {
+            return a - b;
+        })
+    }
+    for (let item in rowsamecol) {
+        for (let i = 0, j = 1; i < rowsamecol[item].length - 1, j < rowsamecol[item].length; i++ , j++) {
+            console.log(rowsamecol[item])
+            if (rowsamecol[item].length > 4 && rowsamecol[item][i] === rowsamecol[item][j] - 1) {
+                rowwcount++;
+                console.log(rowwcount);
+                if (rowwcount === 5) {
+                    alert('게임끝');
+                }
+            } else {
+                rowwcount = 1;
+                console.log(rowwcount);
+            }
+        }
+    }
+    console.log(rowsamecol);
+    console.log(wrows);
+}
+
+function judgediagonal() {
+    let samediagonalcount = 1;
+    ; //같은 col 모으기
+    const samediagonalarry = [];
+    wrows.sort(function (a, b) {
+        if (a[0] === b[0]) {
+            let x = a[1];
+            let y = b[1];
+            return x < y ? -1 : 1; //이중배열정렬
+        }
+        return a[0] - b[0];
+    });
+   
+    console.log(wrows);
+    for (let i=0; i <wrows.length-1; i ++) {
+        samediagonalarry[wrows[i]] = [];
+        for (let j = 1; j < wrows.length; j++){
+           
+            if (wrows[i][0] === wrows[j][0] - 1 && wrows[i][1] === wrows[j][1] - 1 || wrows[i][0] === wrows[j][0] - 2 && wrows[i][1] === wrows[j][1] - 2 || wrows[i][0] === wrows[j][0] - 3 && wrows[i][1] === wrows[j][1] - 3 || wrows[i][0] === wrows[j][0] - 4 && wrows[i][1] === wrows[j][1] - 4) {
+                samediagonalarry[wrows[i]].push(wrows[j]);
+                console.log(samediagonalarry);
+           }
+            if (samediagonalarry[wrows[i]].length >= 4) {
+                alert('게임끝');
+                break;
+            }
+        }
+        console.log(samediagonalcount);
+    }
+}
+    //     } else if (samediagonalarry !== undefined && samediagonalarry[i].length > 0) {
+    //         for (let j of samediagonalarry[i]) {
+    //             let a = samediagonalarry[i][j] + 1;
+    //             let b = samediagonalarry[i + 1];
+    //             if (b.includes(j + 1)) {
+    //                 samediagonalcount++
+    //                 console.log(samediagonalarry, samediagonalcount);
+    //             }
+
+    //         }
+    //     }
+    //     if (samediagonalcount < 5) {
+    //         samediagonalcount = 1;
+    //         console.log(samediagonalcount);
+    //     }
+    //     if (samediagonalcount >= 5) {
+    //         alert('게임끝');
+    //         break;
+    //     }
+    // }
+// }
+// function judgediagonal() {
+//     let samediagonalcount = 1;
+//     ; //같은 col 모으기
+//     const samediagonalarry = [];
+//     wrows.sort(function (a, b) {
+//         if (a[0] === b[0]) {
+//             let x = a[1];
+//             let y = b[1];
+//             return x < y ? -1 : 1; //이중배열정렬
+//         }
+//         return a[0] - b[0];
+//     });
+//     for(let d of wrows){
+//         if (samediagonalarry[d[0]] === undefined){
+//             samediagonalarry[d[0],d[1]] = [];
+//         }
+//         samediagonalarry[d[0], d[1]].push(d[1]);
+//         console.log(samediagonalarry);
+//     }
+//     console.log(wrows);
+//     for (let i = 0; i < samediagonalarry.length; i++) {
+       
+//         if (samediagonalarry[i] === undefined) {
+//             samediagonalcount = 1;
+//         } else if (samediagonalarry !== undefined && samediagonalarry[i].length > 0){
+//             for (let j of samediagonalarry[i]) {
+//             let a = samediagonalarry[i][j]+1;
+//             let b = samediagonalarry[i+1];
+//             if (b.includes(j+1)) {
+//                 samediagonalcount++
+//                 console.log(samediagonalarry, samediagonalcount);
+//             }
+               
+//             }
+//         }
+//         if (samediagonalcount < 5) {
+//             samediagonalcount = 1;
+//             console.log(samediagonalcount);
+//         }
+//        if (samediagonalcount >= 5) {
+//             alert('게임끝');
+//             break;
+//         }
+//     }
+// }
+
+
+
+function judgerow() {
+    wrows.sort(function (a, b) {
+        return a[0] - b[0]; //row값정열
+    });
+    console.log(wrows);
+}

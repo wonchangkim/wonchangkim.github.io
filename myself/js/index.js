be09eae0b05ff61c6e3a99b463b34c2d5e4a3e96
kim wonchang <wonchang.k@gmail.com>
2018-01-04 12:20:20 +0900

    myself update

--- myself/js/index.js
+++ myself/js/index.js
@@ -8,19 +8,33 @@ moment.locale('ko');
 const nowtime = moment().format('LT');
 console.log(nowtime);
 
-let answerArry = [[이름,김원창],[어디,일산살]];
+let answerArry = ['이름',['어디','일산']];
 
 
 function myFunction() {
     let chatarea = document.querySelector('.chat-messages-list');
     var node = document.createElement("div");
+    let awsdiv = document.createElement("div");
     let txt = document.querySelector('#inputres');
+    let awstxt = document.querySelector('.anwtext');
     node.classList.add('block');
     chatarea.appendChild(node);
+    let itxt = txt.value;
     node.innerHTML = `<span class="txttime">${nowtime}</span> 
     <span class="anwtext reschatbubble">${txt.value}</span>`;
     document.querySelector('.inputform').reset();
-
+   
+    if (answerArry.includes(itxt)) {
+        console.log('ddd');
+        chatarea.appendChild(awsdiv);
+       
+        awsdiv.innerHTML = `<div class="anwtxtwrap">
+                                <span class="name">김원창</span>
+                                <span class="anwtext awschatbubble">김원창</span>
+                                <span class= "txttime">${nowtime}</span>
+                            </div>`;
+        
+    }
 }
 
 
